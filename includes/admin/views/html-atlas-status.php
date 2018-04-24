cc936ff3339ef176cdc0e32fa59c561da3304511
Isabel <isabel@isabelcastillo.com>
2018-04-23 21:37:33 -0400

    add link to separate atlas db docs

--- includes/admin/views/html-atlas-status.php
+++ includes/admin/views/html-atlas-status.php
@@ -47,8 +47,8 @@ if ( ! $table_exists ) {
 if ( ! ZP_Atlas_DB::is_installed() && ! ZP_Atlas_DB::is_separate_db() && ! $installing ) {
 	?>
 	<div id="zp-atlas-installer">
-		<p><?php echo __( 'To create your atlas inside your WordPress database, run the Atlas Installer.', 'zodiacpress' ); ?>	
-			<strong><?php _e( 'Skip this to use a separate database.', 'zodiacpress' ); ?></strong></p>
+		<p><?php echo __( 'To create your atlas inside your WordPress database, run the Atlas Installer.', 'zodiacpress' ); ?>
+			<strong><?php printf( __( 'Skip this to use a <a href="%s" target="_blank" rel="noopener">separate database</a>.', 'zodiacpress' ), 'https://cosmicplugins.com/docs/atlas-separate/' ); ?></strong></p>
 		<p><button id="zp-atlas-install" class="button-primary"><?php _e( 'Run the Atlas Installer', 'zodiacpress' ); ?></button></p>
 	</div>
 <?php } ?>
