872d1d41e1963ec04469ef9e3c9ff3b910ce5e7e
Isabel <isabel@isabelcastillo.com>
2018-05-01 12:00:49 -0400

    change class ZP_Report_IDs to ZP_Custom_Reports

--- includes/class-zp-custom-reports.php
+++ includes/class-zp-custom-reports.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * ZodiacPress ZP_Report_IDs class.
+ * ZodiacPress ZP_Custom_Report class.
  *
  * Manages identifiers for all reports.
  *
@@ -10,16 +10,59 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-class ZP_Report_IDs {
+class ZP_Custom_Reports {
 	private static $custom_ids;
 	private static $zp_settings;
+
+	/**
+	 * Gets the tab names for the Custom Reports admin page.
+	 * @return array
+	 */
+	public static function get_tabs() {
+		$tabs = array( 'custom_reports' => __( 'Custom Reports', 'zodiacpress' ) );
+		
+		// @todo if this is going to be called twice, make a property and check it first.
+
+		if ( isset( self::$zp_settings ) ) {
+			$options = self::$zp_settings;
+		} else {
+			$options = get_option( 'zodiacpress_settings' );
+			self::$zp_settings = $options;
+		}
+
+		if ( isset( $options['custom_reports'] ) ) {
+
+			foreach( $options['custom_reports'] as $id => $data ) {
+				$tabs[ $id ] = $data['name'];
+			}
+		}
+
+		return $tabs;
+	}
+
+	/**
+	 * @test every tab, except for the 1st custom reports tab, will have the same sections::
+	 */
+	public static function get_tabs_sections() {
+
+		$sections = array(
+			'main' => __( 'Edit Report', 'zodiacpress'), // @test if this makes sense
+
+			// @todo Orbs section is added ONLY if this report layout includes any aspects!!
+
+			'technical' => __( 'Technical', 'zodiacpress' )
+
+		);
+
+		return $sections;
+	}
 
 	/**
 	 * Gets a list of identifiers for existing custom reports.
 	 *
 	 * @return array List of IDs
 	 */
-	public static function get_custom() {
+	public static function get_ids() {
 		if ( isset( self::$custom_ids ) ) {
 			return self::$custom_ids;
 		}
@@ -30,8 +73,6 @@ class ZP_Report_IDs {
 			$options = get_option( 'zodiacpress_settings' );
 			self::$zp_settings = $options;
 		}
-
-		// @todo make sure "$options['custom_reports']" is always an array, even before any custom report ids exist, so be sure settings page saves this as an empty array (not a blank or empty string).
 
 		self::$custom_ids = isset( $options['custom_reports'] ) ? array_keys( $options['custom_reports'] ) : array();
 		return self::$custom_ids;
@@ -47,7 +88,7 @@ class ZP_Report_IDs {
 		foreach ( zp_get_planets() as $p ) {
 			$core_ids[] = 'planet_lookup_' . $p['id'];
 		}
-		return ( in_array( $id, self::get_custom() ) || in_array( $id, $core_ids ) );
+		return ( in_array( $id, self::get_ids() ) || in_array( $id, $core_ids ) );
 	}
 
 	/**
