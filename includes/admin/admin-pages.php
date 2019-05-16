ac7e22a36aac2a0106b275be806b043b0cb66886
Isabel <isabel@isabelcastillo.com>
2019-05-16 16:46:35 -0400

    remove unnecessary licensing from header

--- includes/admin/admin-pages.php
+++ includes/admin/admin-pages.php
@@ -3,23 +3,17 @@
  * Admin Pages
  *
  * @package     ZodiacPress
- * @subpackage  Admin/Pages
- * @copyright   Copyright (c) 2016-2017, Isabel Castillo
- * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  */
 if ( ! defined( 'ABSPATH' ) ) exit;
-
 /**
  * Creates the ZodiacPress admin pages
  */
 function zp_add_admin_pages() {
-
 	add_menu_page( __( 'ZodiacPress', 'zodiacpress' ), __( 'ZodiacPress', 'zodiacpress' ), 'manage_zodiacpress_interps', 'zodiacpress', 'zp_interpretations_page', 'dashicons-universal-access-alt', '21.9' );
 
 	add_submenu_page( 'zodiacpress', __( 'ZodiacPress Tools', 'zodiacpress' ), __( 'Tools', 'zodiacpress' ), 'manage_zodiacpress_settings', 'zodiacpress-tools', 'zp_tools_page' );
 
 	add_submenu_page( 'zodiacpress', __( 'ZodiacPress Settings', 'zodiacpress' ), __( 'Settings', 'zodiacpress' ), 'manage_zodiacpress_settings', 'zodiacpress-settings', 'zp_options_page' );
-
 }
 add_action( 'admin_menu', 'zp_add_admin_pages' );
 /**
